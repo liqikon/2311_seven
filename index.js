32046984176a452bc7c247301fbfffd65e167d6d
Jieun Lee <vjzzing@gmail.com>
2023-06-30 22:10:48 +0900

    test

--- index.js
+++ index.js
@@ -1,135 +1,85 @@
 
-const resolver = {
-    resolve: function resolve(options, callback) {
-        // The string to resolve
-        const resolveString = options.resolveString || options.element.getAttribute('data-target-resolver');
-        const combinedOptions = Object.assign({}, options, {
-            resolveString: resolveString
-        });
 
-        function getRandomInteger(min, max) {
-            return Math.floor(Math.random() * (max - min + 1)) + min;
-        };
+const playButton = document.getElementById('playButton');
+const pauseButton = document.getElementById('pauseButton');
+const audioPlayer = document.getElementById("audio-player");
+const disk = document.querySelector('.disk');
 
-        function randomCharacter(characters) {
-            return characters[getRandomInteger(0, characters.length - 1)];
-        };
+playButton.addEventListener('click', function() {
+  if (playButton.classList.contains('on')) {
+    playButton.classList.remove('on');
+    audioPlayer.pause();
+    disk.classList.remove('rotate');
+    audioPlayer.currentTime = 0;
+  } else {
+    playButton.classList.add('on');
+    audioPlayer.play();
+    disk.classList.add('rotate');
+    pauseButton.classList.remove('on');
+  }
+});
 
-
-        function doRandomiserEffect(options, callback) {
-            const characters = options.characters;
-            const element = options.element;
-            const partialString = options.partialString;
-
-            if (partialString.length === options.resolveString.length) {
-                element.textContent = options.resolveString;
-                if (typeof callback === "function") {
-                    callback();
-                }
-            } else {
-                let newString = '';
-                for (let i = 0; i < options.resolveString.length; i++) {
-                    newString += randomCharacter(characters);
-                }
-                element.textContent = newString;
-                if (typeof callback === "function") {
-                    callback();
-                }
-            }
-        }
+pauseButton.addEventListener('click', function() {
+  pauseButton.classList.toggle('on');
+  
+  if (audioPlayer.paused) {
+ 
+  } else {
+    audioPlayer.pause(); // 노래 일시정지
+    disk.classList.remove('rotate');
+    playButton.classList.remove('on');
+  }
+});
 
 
-        function doResolverEffect(options, callback) {
-            const resolveString = options.resolveString;
-            const characters = options.characters;
-            const offset = options.offset;
-            const partialString = resolveString.substring(0, offset);
-            const combinedOptions = Object.assign({}, options, {
-                partialString: partialString
-            });
+const timeline = document.querySelector(".timeline");
+const timelineProgress = document.createElement("div");
+timelineProgress.classList.add("timeline-progress");
+timeline.appendChild(timelineProgress);
 
-            if (offset <= resolveString.length) {
-                let newString = '';
-                for (let i = 0; i < resolveString.length; i++) {
-                    if (i < offset) {
-                        newString += resolveString[i];
-                    } else {
-                        newString += randomCharacter(characters);
-                    }
-                }
-                options.element.textContent = newString;
+audioPlayer.addEventListener("timeupdate", function() {
+  const currentTime = audioPlayer.currentTime;
+  const duration = audioPlayer.duration;
+  const progressPercentage = (currentTime / duration) * 100;
+  timelineProgress.style.width = progressPercentage + "%";
+});
 
-                setTimeout(() => {
-                    const nextOptions = Object.assign({}, options, {
-                        offset: offset + 1
-                    });
-                    doResolverEffect(nextOptions, callback);
-                }, options.timeout);
-            } else if (typeof callback === "function") {
-                callback();
-            }
-        }
-
-
-        doResolverEffect(combinedOptions, callback);
-     
-    }
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds < 10 ? "0" + seconds : seconds}`;
 }
 
-/* Some GLaDOS quotes from Portal 2 chapter 9: The Part Where He Kills You
- * Source: http://theportalwiki.com/wiki/GLaDOS_voice_lines#Chapter_9:_The_Part_Where_He_Kills_You
- */
-const strings = [
-    'myself',
-   
-];
+audioPlayer.addEventListener("timeupdate", function() {
+  const currentTime = audioPlayer.currentTime;
+  const formattedTime = formatTime(currentTime);
+  const timeElement = document.querySelector(".time");
+  timeElement.textContent = formattedTime;
+});
 
-let counter = 0;
 
-const options = {
-    // Initial position
-    offset: 0,
-    // Timeout between each random character
-    timeout: 80,
-    // Number of random characters to show
-    iterations: 10,
-    // Random characters to pick from
-    characters: ['$', '3', '8', 'd', '_', 'f', 'g', '!', '5', 'j', 'k', 'l', '2', 'n', '_', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'x', '#', '%', '&', '-', '+', '_', '?', '/', '\\', '='],
-    // String to resolve
-    resolveString: strings[counter],
-    // The element
-    element: document.querySelector('[data-target-resolver]')
-}
+const volumeBar = document.querySelector('.volume-bar');
+const volumeProgress = document.querySelector('.volume-progress');
 
-// Callback function when resolve completes
-function callback() {
-    setTimeout(() => {
-        counter++;
+volumeBar.addEventListener('mousedown', function(event) {
+  event.preventDefault();
+  const barHeight = this.clientHeight;
+  const offsetY = event.clientY - this.getBoundingClientRect().top;
+  const initialVolume = audioPlayer.volume;
+  
+  function adjustVolume(event) {
+    const newOffsetY = event.clientY - volumeBar.getBoundingClientRect().top;
+    let volume = 1 - (newOffsetY / barHeight);
+    volume = Math.max(0, Math.min(1, volume)); // Clamp volume between 0 and 1
+    audioPlayer.volume = volume;
+    volumeProgress.style.top = ((1 - volume) * 100) + '%';
+  }
 
-        if (counter >= strings.length) {
-            counter = 0;
-        }
+  function stopAdjustingVolume() {
+    document.removeEventListener('mousemove', adjustVolume);
+    document.removeEventListener('mouseup', stopAdjustingVolume);
+  }
 
-        let nextOptions = Object.assign({}, options, {
-            resolveString: strings[counter]
-        });
-        resolver.resolve(nextOptions, callback);
-    }, 1500);
-}
-// 이벤트 핸들러 함수
-function handleSubmit() {
-    const input = document.querySelector("#string-input");
-    const newString = input.value;
-
-    // 새로운 문자열을 strings 배열에 추가
-    strings.push(newString);
-
-    // 입력값 초기화
-    input.value = "";
-}
-
-// Submit 버튼 클릭 이벤트 리스너 등록
-const submitBtn = document.querySelector("#submit-btn");
-submitBtn.addEventListener("click", handleSubmit);
-
-resolver.resolve(options, callback);
+  document.addEventListener('mousemove', adjustVolume);
+  document.addEventListener('mouseup', stopAdjustingVolume);
+});
